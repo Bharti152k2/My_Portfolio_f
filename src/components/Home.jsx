d854cf15c6d4df93ee751a315493994a383be802
Bharti152k2 <nidhi15sak@gmail.com>
2025-02-02 16:37:14 +0530

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import B3 from "../assets/F.jpg";
 import B4 from "../assets/G.jpg";
 import B5 from "../assets/H.jpg";
 import B6 from "../assets/K.jpg";
+import B7 from "../assets/N.jpg";
 function Home() {
   return (
     <section className="home-banner">
@@ -14,15 +15,20 @@ function Home() {
         <h1>I'm Bharti</h1>
         <h1>Full Stack Developer</h1>
         <p>
-          A passionate MERN stack developer dedicated to building exceptional
-          web applications. I transform ideas into reality <br /> using MERN. With a
-          keen eye for detail and a problem-solving mindset, I create solutions
-          that make an impact.
+          A passionate MERN stack developer dedicated to building exceptional{" "}
+          <br />
+          web applications. I transform ideas into reality using MERN. <br />
+          With a keen eye for detail and a problem-solving mindset, I create{" "}
+          <br />
+          solutions that make an impact.
         </p>
         <div className="btns">
           <button>Resume Download</button>
           <button>Let's Talk</button>
         </div>
+      </article>
+      <article>
+        <img src={B7} alt="" width="500px" />
       </article>
     </section>
   );
